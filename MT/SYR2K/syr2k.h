ebbddce1c26ba5a65dada8545eeb31663827d9ab
Huaijin Li <578761491@qq.com>
2025-02-26 17:11:01 +0800

    add SYR2K, remove CUDA version

--- MT/SYR2K/syr2k.h
+++ MT/SYR2K/syr2k.h
@@ -1,15 +1,6 @@
-/**
- * syrk.cuh: This file is part of the PolyBench/GPU 1.0 test suite.
- *
- *
- * Contact: Scott Grauer-Gray <sgrauerg@gmail.com>
- * Will Killian <killian@udel.edu>
- * Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
- * Web address: http://www.cse.ohio-state.edu/~pouchet/software/polybench/GPU
- */
 
-#ifndef SYRK_H
-# define SYRK_H
+#ifndef SYR2K_H
+# define SYR2K_H
 
 /* Default to STANDARD_DATASET. */
 # if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
@@ -20,18 +11,18 @@
 # if !defined(N)
 /* Define the possible dataset sizes. */
 #  ifdef MINI_DATASET
-#define NI 1024
-#define NJ 1024
+#define NI 128
+#define NJ 128
 #  endif
 
 #  ifdef SMALL_DATASET
-#define NI 1024
-#define NJ 1024
+#define NI 256
+#define NJ 256
 #  endif
 
 #  ifdef STANDARD_DATASET /* Default if unspecified. */
-#define NI 1024
-#define NJ 1024
+#define NI 512
+#define NJ 512
 #  endif
 
 #  ifdef LARGE_DATASET
@@ -40,8 +31,8 @@
 #  endif
 
 #  ifdef EXTRALARGE_DATASET
-#define NI 1024
-#define NJ 1024
+#define NI 2048
+#define NJ 2048
 #  endif
 # endif /* !N */
 
@@ -49,7 +40,7 @@
 # define _PB_NJ POLYBENCH_LOOP_BOUND(NJ,nj)
 
 # ifndef DATA_TYPE
-#  define DATA_TYPE float
+#  define DATA_TYPE double 
 #  define DATA_PRINTF_MODIFIER "%0.2lf "
 # endif
 
@@ -57,5 +48,10 @@
 #define DIM_THREAD_BLOCK_X 32
 #define DIM_THREAD_BLOCK_Y 8
 
-
-#endif /* !SYRK*/
+/* DSP config */
+#define THREAD_NUM 24
+static int nthreads = THREAD_NUM;                 // 申请的线程组的线程数
+static int clusterId = 0;                         // 申请的cluster的ID，有0 - 3，共四个加速簇
+static const char *devProgram = "./syr2k.dev.dat"; // 设备端二进制文件的位置
+static const char *kernel = "syr2k_kernel";        // 核函数名称
+#endif /* !SYR2K*/
